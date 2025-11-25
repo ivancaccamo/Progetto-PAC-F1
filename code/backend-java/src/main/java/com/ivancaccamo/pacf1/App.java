270c38daa3f7a3191f6866796de9bf10e1012800
Abirolini11 <a.birolini6@studenti.unibg.it>
2025-11-25 11:55:48 +0100

    restuctured project

---
+++ code/backend-java/src/main/java/com/ivancaccamo/pacf1/App.java
@@ -0,0 +1,7 @@
+package com.ivancaccamo.pacf1;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("PAC-F1 Backend Java avviato!");
+    }
+}
---
+++ code/backend-java/src/test/java/com/ivancaccamo/pacf1/AppTest.java
@@ -0,0 +1,11 @@
+package com.ivancaccamo.pacf1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testMain() {
+        assertEquals(2, 1 + 1);
+    }
+}
